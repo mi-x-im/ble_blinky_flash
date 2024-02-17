e8010d8fe3db31a1eb595b6792c243fe0a34a677
mi_x_im <105375136+mi-x-im@users.noreply.github.com>
2024-02-17 12:52:06 +0300

    Update main.c

--- src/main.c
+++ src/main.c
@@ -1,87 +1,204 @@
-/*
- * Copyright (c) 2020 Intel Corporation
- *
- * SPDX-License-Identifier: Apache-2.0
- */
+#include <zephyr/logging/log.h>
+#include <zephyr/bluetooth/bluetooth.h>
+#include <zephyr/bluetooth/uuid.h>
+#include <zephyr/bluetooth/gatt.h>
+#include <zephyr/bluetooth/hci.h>
+#include <zephyr/drivers/gpio.h>
+#include <zephyr/drivers/flash.h>
 
-#include <zephyr/kernel.h>
-#include <zephyr/arch/cpu.h>
-#include <zephyr/sys/arch_interface.h>
+#define LOG_MODULE_NAME app
+LOG_MODULE_REGISTER(LOG_MODULE_NAME);
 
-#define NUM_THREADS 20
-#define STACK_SIZE (1024)
+#define DEVICE_NAME CONFIG_BT_DEVICE_NAME
+#define DEVICE_NAME_LEN (sizeof(DEVICE_NAME)-1)
 
-K_THREAD_STACK_ARRAY_DEFINE(tstacks, NUM_THREADS, STACK_SIZE);
+static K_SEM_DEFINE(bt_init_ok, 1, 1);
+static struct bt_conn *current_conn;
 
-static struct k_thread t[NUM_THREADS];
+#define BT_UUID_REMOTE_SERV_VAL \
+	BT_UUID_128_ENCODE(0xe9ea0001, 0xe19b, 0x482d, 0x9293, 0xc7907585fc48)
+#define BT_UUID_REMOTE_MESSAGE_CHRC_VAL \
+	BT_UUID_128_ENCODE(0xe9ea0003, 0xe19b, 0x482d, 0x9293, 0xc7907585fc48)
 
-K_MUTEX_DEFINE(mutex);
-K_CONDVAR_DEFINE(condvar);
+#define BT_UUID_REMOTE_SERVICE          BT_UUID_DECLARE_128(BT_UUID_REMOTE_SERV_VAL)
+#define BT_UUID_REMOTE_MESSAGE_CHRC     BT_UUID_DECLARE_128(BT_UUID_REMOTE_MESSAGE_CHRC_VAL)
 
-static int done;
+struct bt_remote_service_cb {
+    void (*data_received)(struct bt_conn *conn, const uint8_t *const data, uint16_t len);
+};
 
-void worker_thread(void *p1, void *p2, void *p3)
+static const struct bt_data ad[] = {
+    BT_DATA_BYTES(BT_DATA_FLAGS, (BT_LE_AD_GENERAL | BT_LE_AD_NO_BREDR)),
+    BT_DATA(BT_DATA_NAME_COMPLETE, DEVICE_NAME, DEVICE_NAME_LEN)
+};
+
+static const struct bt_data sd[] = {
+    BT_DATA_BYTES(BT_DATA_UUID128_ALL, BT_UUID_REMOTE_SERV_VAL),
+};
+
+static const struct gpio_dt_spec led0 = GPIO_DT_SPEC_GET(DT_ALIAS(led0), gpios);
+static struct device *flash_dev = DEVICE_DT_GET(DT_NODELABEL(flash_controller));
+typedef struct inf
 {
-	const int myid = (long)p1;
-	const int workloops = 5;
+	uint8_t state;
+} inf;
+static inf my_info;
 
-	for (int i = 0; i < workloops; i++) {
-		printk("[thread %d] working (%d/%d)\n", myid, i, workloops);
-		k_sleep(K_MSEC(500));
-	}
+static ssize_t on_write(struct bt_conn *conn, const struct bt_gatt_attr *attr, const void *buf, uint16_t len, uint16_t offset, uint8_t flags);
 
-	/*
-	 * we're going to manipulate done and use the cond, so we need the mutex
-	 */
-	k_mutex_lock(&mutex, K_FOREVER);
+BT_GATT_SERVICE_DEFINE(remote_srv,
+BT_GATT_PRIMARY_SERVICE(BT_UUID_REMOTE_SERVICE),
+    BT_GATT_CHARACTERISTIC(BT_UUID_REMOTE_MESSAGE_CHRC,
+                    BT_GATT_CHRC_WRITE_WITHOUT_RESP,
+                    BT_GATT_PERM_READ | BT_GATT_PERM_WRITE,
+                    NULL, on_write, NULL),
+);
 
-	/*
-	 * increase the count of threads that have finished their work.
-	 */
-	done++;
-	printk("[thread %d] done is now %d. Signalling cond.\n", myid, done);
+static struct bt_conn_cb bluetooth_callbacks;
+static struct bt_remote_service_cb remote_callbacks;
 
-	k_condvar_signal(&condvar);
-	k_mutex_unlock(&mutex);
+static const uint32_t offset = 0xf8000;
+
+
+void download_info()
+{
+    load_info_state();
+    gpio_pin_set_dt(&led0, my_info.state);
 }
 
-int main(void)
+void save_led_state(inf my_info)
 {
-	k_tid_t tid[NUM_THREADS];
+    printk("LED state: %d\n", my_info.state);
+    flash_erase(flash_dev, offset, 4096);
+    flash_write(flash_dev, offset, &my_info, sizeof(inf));
+    flash_read(flash_dev, offset, &my_info, sizeof(inf));
+}
 
-	done = 0;
+void write_info()
+{
+	my_info.state = !(my_info.state);
+	save_led_state(my_info);
+	gpio_pin_set_dt(&led0, my_info.state);
+    
+}
 
-	for (int i = 0; i < NUM_THREADS; i++) {
-		tid[i] =
-			k_thread_create(&t[i], tstacks[i], STACK_SIZE,
-					worker_thread, INT_TO_POINTER(i), NULL,
-					NULL, K_PRIO_PREEMPT(10), 0, K_NO_WAIT);
+void load_info_state()
+{
+    flash_read(flash_dev, offset, &my_info, sizeof(inf));
+}
+
+static ssize_t on_write(struct bt_conn *conn,
+			  const struct bt_gatt_attr *attr,
+			  const void *buf,
+			  uint16_t len,
+			  uint16_t offset,
+			  uint8_t flags)
+{
+	LOG_INF("Received data, handle %d, conn %p",
+		attr->handle, (void *)conn);
+
+	LOG_INF("Received data as text: %.*s", len, (char *)buf);
+	if (strncmp((char *)buf, "start", len) == 0) {
+        // Включаем светодиод
+        gpio_pin_set_dt(&led0, 1);
+		write_info();
+    } else if (strncmp((char *)buf, "stop", len) == 0) {
+        // Выключаем светодиод
+        gpio_pin_set_dt(&led0, 0);
+		write_info();
+
+    }
+	if (remote_callbacks.data_received) {
+		remote_callbacks.data_received(conn, buf, len);
+    }
+	return len;
+}
+
+void on_connected(struct bt_conn *conn, uint8_t err)
+{
+	if(err) {
+		LOG_ERR("connection err: %d", err);
+		return;
 	}
-	k_sleep(K_MSEC(1000));
+	LOG_INF("Connected.");
+	current_conn = bt_conn_ref(conn);
+}
 
-	k_mutex_lock(&mutex, K_FOREVER);
-
-	/*
-	 * are the other threads still busy?
-	 */
-	while (done < NUM_THREADS) {
-		printk("[thread %s] done is %d which is < %d so waiting on cond\n",
-		       __func__, done, (int)NUM_THREADS);
-
-		/* block this thread until another thread signals cond. While
-		 * blocked, the mutex is released, then re-acquired before this
-		 * thread is woken up and the call returns.
-		 */
-		k_condvar_wait(&condvar, &mutex, K_FOREVER);
-
-		printk("[thread %s] wake - cond was signalled.\n", __func__);
-
-		/* we go around the loop with the lock held */
+void on_disconnected(struct bt_conn *conn, uint8_t reason)
+{
+	LOG_INF("Disconnected (reason: %d)", reason);
+	if(current_conn) {
+		bt_conn_unref(current_conn);
+		current_conn = NULL;
 	}
+}
 
-	printk("[thread %s] done == %d so everyone is done\n",
-		__func__, (int)NUM_THREADS);
+void bt_ready(int err)
+{
+    if (err) {
+        LOG_ERR("bt_ready returned %d", err);
+    }
+    k_sem_give(&bt_init_ok);
+}
 
-	k_mutex_unlock(&mutex);
-	return 0;
+int bluetooth_init(struct bt_conn_cb *bt_cb, struct bt_remote_service_cb *remote_cb)
+{
+    int err;
+    LOG_INF("Initializing bluetooth...");
+
+    if (bt_cb == NULL || remote_cb == NULL) {
+        return -NRFX_ERROR_NULL;
+    }
+
+    bt_conn_cb_register(bt_cb);
+    remote_callbacks.data_received = remote_cb->data_received;
+
+    err = bt_enable(bt_ready);
+    if (err) {
+        LOG_ERR("bt_enable returned %d", err);
+        return err;
+    }
+    
+    k_sem_take(&bt_init_ok, K_FOREVER);
+
+    err = bt_le_adv_start(BT_LE_ADV_CONN, ad, ARRAY_SIZE(ad), sd, ARRAY_SIZE(sd));
+    if (err) {
+        LOG_ERR("Couldn't start advertising (err = %d)", err);
+        return err;
+    }
+    return err;
+}
+
+void check_device_led()
+{
+    if (!device_is_ready(led0.port))
+    {
+        printk("GPIO device is not ready\r\n");
+    }
+}
+
+void config_led()
+{
+    gpio_pin_configure_dt(&led0, GPIO_OUTPUT_ACTIVE);
+}
+
+void main(void)
+{
+	int err;
+	LOG_INF("Hello World! %s\n", CONFIG_BOARD);
+	check_device_led();
+	config_led();
+	download_info();
+	bluetooth_callbacks.connected = on_connected;
+	bluetooth_callbacks.disconnected = on_disconnected;
+
+	err = bluetooth_init(&bluetooth_callbacks, &remote_callbacks);
+	if (err) {
+		LOG_ERR("bt_enable returned %d", err);
+	}	
+
+	LOG_INF("Running...");
+	for (;;) {
+		k_sleep(K_MSEC(1000));
+	}
 }
